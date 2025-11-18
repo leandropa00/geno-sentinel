9445a9b162ad2a11562b02e298ed736dd359033d
Leandro Panesso Atehortúa <leandro@queo.com.co>
2025-11-17 21:03:02 -0500

    feat: :sparkles: add LoginRequest and LoginResponse DTOs for user authentication

---
+++ api-gateway/src/main/java/com/genosentinel/gateway/dto/LoginResponse.java
@@ -0,0 +1,32 @@
+package com.genosentinel.gateway.dto;
+
+public class LoginResponse {
+
+    private String token;
+    private String type;
+
+    public LoginResponse() {
+    }
+
+    public LoginResponse(String token, String type) {
+        this.token = token;
+        this.type = type;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
+
